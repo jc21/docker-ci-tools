cf35f0b55fa66bd015a75ca15d1dae108849ea60
Jamie Curnow <jc@jc21.com>
2023-08-01 14:42:42 +1000

    Switch to debian

--- Dockerfile
+++ Dockerfile
@@ -1,47 +1,12 @@
-#===============
-# gobuild
-#===============
-
-FROM --platform=${TARGETPLATFORM:-linux/amd64} jc21/rpmbuild-centos8:golang AS gobuild
-
-ARG GOPROXY
-ARG GOPRIVATE
-
-ENV GOPROXY=$GOPROXY \
-	GOPRIVATE=$GOPRIVATE \
-	GO111MODULE=on \
-	CGO_ENABLED=1
-
-WORKDIR /workspace
-RUN git clone https://github.com/cli/cli.git
-WORKDIR /workspace/cli
-RUN make
-
-#===============
-# Final image
-#===============
-
-FROM --platform=${TARGETPLATFORM:-linux/amd64} centos:8
+FROM --platform=${TARGETPLATFORM:-linux/amd64} debian:bookworm-slim
 
 LABEL maintainer="Jamie Curnow <jc@jc21.com>"
 
-# Packages
-COPY --from=gobuild /workspace/cli/bin/gh /usr/bin/gh
-RUN dnf -y install epel-release
-RUN dnf -y install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
-RUN dnf -y install https://yum.jc21.com/jc21-yum.rpm
-RUN dnf -y install 'dnf-command(config-manager)'
-RUN dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-RUN dnf -y module enable php:remi-7.4
-RUN dnf -y update
-RUN dnf -y install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
-RUN dnf -y install docker-ce
-RUN dnf -y install which git wget rpmdevtools rpmlint yum-utils expect s3cmd python3-pip createrepo python3-magic rpm-sign jq hugo php-cli php-common
-RUN curl -sfL https://install.goreleaser.com/github.com/tj/node-prune.sh | bash -s -- -b /usr/local/bin
-RUN dnf clean all
-RUN rm -rf /var/cache/yum
-RUN mkdir -p /data
-RUN pip3 install --upgrade pip
-RUN pip3 install awscli
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 
+COPY scripts/install-tools.sh /tmp/install-tools.sh
+RUN /tmp/install-tools.sh \
+	&& rm -f /tmp/install-tools.sh
+
+RUN mkdir -p /data
 WORKDIR /data
---
+++ scripts/install-tools.sh
@@ -0,0 +1,63 @@
+#!/bin/bash -e
+
+S3CMD_VERSION=2.3.0
+
+# pkgs
+echo '+-------------------------+'
+echo '| packages                |'
+echo '+-------------------------+'
+apt update
+apt install -y --no-install-recommends \
+	awscli \
+	ca-certificates \
+	curl \
+	git \
+	hugo \
+	jq \
+	openssl \
+	wget \
+	which \
+	php-cli \
+	php-common
+
+# s3cmd
+echo '+-------------------------+'
+echo '| s3cmd                   |'
+echo '+-------------------------+'
+wget -O /tmp/s3cmd.tgz "https://github.com/s3tools/s3cmd/releases/download/v${S3CMD_VERSION}/s3cmd-${S3CMD_VERSION}.tar.gz"
+cd /tmp
+tar -xzf s3cmd.tgz
+mv /tmp/s3cmd-${S3CMD_VERSION}/s3cmd /bin/
+rm -rf /tmp/s3cmd*
+
+# node-prune
+echo '+-------------------------+'
+echo '| node-prune              |'
+echo '+-------------------------+'
+curl -sf https://gobinaries.com/tj/node-prune | sh
+
+# docker
+echo '+-------------------------+'
+echo '| docker                  |'
+echo '+-------------------------+'
+curl -sfL https://get.docker.com/ | sh
+apt remove -y docker-ce
+apt autoremove -y
+
+# gh cli
+echo '+-------------------------+'
+echo '| github cli              |'
+echo '+-------------------------+'
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update
+apt install gh -y
+
+# cleanup
+echo '+-------------------------+'
+echo '| cleanup                 |'
+echo '+-------------------------+'
+apt clean
+rm -rf /var/lib/apt/lists/*
+rm -rf /var/cache/* /var/log/* /tmp/* /var/lib/dpkg/status-old
